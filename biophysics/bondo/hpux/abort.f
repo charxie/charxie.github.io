c8222154f57a0612295ee909ffed486f7e4c5781
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-12 14:37:20 +0800

    Delete biophysics directory

--- biophysics/bondo/hpux/abort.f
+++
@@ -1,7 +0,0 @@
-      SUBROUTINE ABORT(WORD)                                                    
-      IMPLICIT DOUBLE PRECISION (A-H,O-Z)                                       
-      PRINT 1000,WORD                                                           
-      CALL EXIT                                                                 
-      RETURN                                                                    
- 1000 FORMAT(//,'***  ERROR EXIT0 ',A6,'  ***')                                 
-      END                                                                       
--- biophysics/bondo/linux/fact.f
+++
@@ -1,9 +0,0 @@
-      FUNCTION FACT(N)
-      IMPLICIT DOUBLE PRECISION (A-H,O-Z)
-      PRODT = 1.D0
-      IF(N.EQ.0)GO TO 30
-   10 DO 20 I=1,N
-   20 PRODT=PRODT*dble(I)
-   30 FACT=PRODT
-      return
-      END
